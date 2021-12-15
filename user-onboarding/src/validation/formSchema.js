10361458ee46b29511f90e96b4502e58ee2550c6
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-12-15 15:05:35 -0700

    Terms of Service checkbox must be clicked to submit new user

--- user-onboarding/src/validation/formSchema.js
+++ user-onboarding/src/validation/formSchema.js
@@ -1,21 +1,22 @@
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
-  name: yup.string().required("Username is a required field"),
+  name: yup.string().required("***Username is a required field***"),
   username: yup
     .string()
     .trim()
-    .required("Username is a required field")
-    .min(5, "Username has to be 5 characters long"),
-  email: yup.string().trim().required("Email is a required field"),
+    .required("***Username is a required field***")
+    .min(5, "***Username has to be 5 characters long***"),
+  email: yup.string().trim().required("***Email is a required field***"),
   password: yup
     .string()
     .trim()
-    .required("Password is a required field")
-    .min(6, "Password must have at least 6 characters"),
+    .required("***Password is a required field***")
+    .min(6, "***Password must have at least 6 characters***"),
   tos: yup
     .boolean()
-    .required("Terms of Service must be checked to create a new user"), //ask Jesse how to make this work
+    .required("***Terms of Service must be accepted***")
+    .oneOf([true], "***Terms of Service must be accepted***"),
 });
 
 export default formSchema;
