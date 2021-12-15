c43135d5c6ea8cc3b76a2b6724b085ca291d1665
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-12-15 13:29:07 -0700

    created basic form layout

--- user-onboarding/component/Form.js
+++ user-onboarding/component/Form.js
@@ -1,1 +1,3 @@
-export default Form;
+import React from "react";
+
+export default function Form() {}
--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import React, { useState } from "react";
-import Form from "../component/Form";
-import axios from "axios";
-import * as yup from "yup";
+// import Form from "../component/Form";
+// import schema from '../validation/formSchema';
+// import axios from "axios";
+// import * as yup from "yup";
 
 const initialFormValues = {
   username: "",
@@ -13,23 +14,60 @@ const initialFormValues = {
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Users</h1>
       </header>
+      <div>
+        <form className="form-container">
+          <div className="form-group">
+            <h2>Create a New User</h2>
+            <button>Submit</button>
+            <div className="inputs-container">
+              <label>
+                Username
+                <input
+                  name="username"
+                  type="text"
+                  // value={values.username}
+                  // onChange={onChange}
+                />
+              </label>
+              <label>
+                Email
+                <input
+                  name="email"
+                  type="email"
+                  // value={values.email}
+                  // onChange={onChange}
+                />
+              </label>
+              <label>
+                Password
+                <input
+                  name="password"
+                  type="password"
+                  // value={values.password}
+                  // onChange={onChange}
+                />
+              </label>
+              <label>
+                Terms Of Service
+                <input
+                  name="tos"
+                  type="checkbox"
+                  checked={values.tos}
+                  onChange={onChange}
+                />
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
+      <h2>User List</h2>
     </div>
   );
 }
