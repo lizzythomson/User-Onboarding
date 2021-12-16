979d10e94ce83b0ae4480135836067897ff42dba
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-12-16 14:35:40 -0700

    created tests to verify that user can type in all inputs, that tos can be checked, that the submit button is initially disabled, that the submit button is enabled when all inputs are filled out and tos is checked

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -22,4 +22,49 @@ describe("Form Test", () => {
     tosInput().should("exist");
     submitBtn().should("exist");
   });
+
+  describe("Filling out the inputs", () => {
+    it("correctly navigates to the site", () => {
+      cy.url().should("include", "localhost");
+    });
+
+    it("can type in the inputs", () => {
+      nameInput()
+        .should("have.value", "")
+        .type("Jello")
+        .should("have.value", "Jello");
+      userNameInput()
+        .should("have.value", "")
+        .type("CoolUserName")
+        .should("have.value", "CoolUserName");
+      emailInput()
+        .should("have.value", "")
+        .type("catchyemail@gmail.com")
+        .should("have.value", "catchyemail@gmail.com");
+      passwordInput()
+        .should("have.value", "")
+        .type("myPassword*")
+        .should("have.value", "myPassword*");
+    });
+
+    it("submit button starts out disabled", () => {
+      submitBtn().should("be.disabled");
+    });
+
+    it("Terms of Services is able to be checked", () => {
+      tosInput()
+        .should("not.be.checked")
+        .check({ force: true })
+        .should("be.checked");
+    });
+
+    it("submit button enables when all inputs are filled out", () => {
+      nameInput().type("Jane Doe");
+      userNameInput().type("janedoe13");
+      emailInput().type("janekatedoe14@gmail.com");
+      passwordInput().type("myPassword*");
+      tosInput().check({ force: true });
+      submitBtn().should("not.be.disabled");
+    });
+  });
 });
